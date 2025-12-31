f32ccd5291739ca636cf6771e6694844dbd2b392
sumit <sumitsingh12013@gmail.com>
2025-12-31 14:11:50 +0000

    feat: Add SQL queries demonstrating string and data type manipulation, including various functions and exercises.

---
+++ tasks/task_2.py
@@ -0,0 +1,170 @@
+from execute_sql import main
+
+
+executor = main()
+
+
+## DATA Generation, Manipukation and Conversion
+## DATA Types :- CHAR, VARCHAR, AND TEXT
+
+
+# Create table
+query = """
+CREATE TABLE string_tbl
+(char_fld CHAR(30),
+vchar_fld VARCHAR(30),
+text_fld TEXT
+)
+"""
+
+
+# String Generation
+query = """
+INSERT INTO string_tbl (char_fld, vchar_fld, text_fld)
+VALUES ('this is char data', 'this is varchar data', 'this is text data');
+"""
+
+
+# Check limit
+query = """
+UPDATE string_tbl
+SET vchar_fld = '***************************************************************************************************';
+"""
+
+
+# Check mode
+query = """
+SELECT @@session.sql_mode;
+"""
+# Columns: @@session.sql_mode
+# [('ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION',)]
+
+
+
+# Update mode 
+query = """
+SET sql_mode='ansi';
+"""
+## need to pass for every session
+
+
+# Special character 
+query = """
+UPDATE string_tbl
+SET text_fld = 'It won't work';
+"""
+
+
+# Use double slashes to escape things
+query = """
+UPDATE string_tbl
+SET text_fld = 'Now it\\'s working with double slashes';
+"""
+
+
+query = """
+SELECT *
+FROM string_tbl;
+"""
+
+
+# Using safe quote function to fetch data
+query = """
+SELECT quote(text_fld)
+FROM string_tbl;
+"""
+
+
+# Concatenate function
+query = """
+SELECT CONCAT('danke sch',CHAR(148),'n');
+"""
+
+
+# ASCII func
+query = """
+SELECT ASCII('b');
+"""
+
+
+## STRING Manipulation
+# Delete
+query = """
+DELETE FROM string_tbl;
+"""
+
+query = """
+SELECT *
+FROM string_tbl;
+"""
+
+query = """
+INSERT INTO string_tbl (char_fld, vchar_fld, text_fld)
+VALUES ('this is char data', 'this is varchar data', 'this is text data');
+"""
+
+# Length
+query = """
+SELECT LENGTH(char_fld) char_len, LENGTH(vchar_fld) vchar_len, LENGTH(text_fld) text_len
+FROM string_tbl;"""
+
+# Find position (1st occurance) position starts with 1, if 0 then not found
+query = """
+SELECT POSITION('text' IN text_fld)
+FROM string_tbl;"""
+
+# locate it has 3 positional args 3rd to start the search from which position
+query = """
+SELECT LOCATE('is', text_fld, 5)
+FROM string_tbl;"""
+
+
+# string comparisons
+query = """
+SELECT STRCMP('12345','12345') 12345_12345,
+STRCMP('abcd','xyz') abcd_xyz,
+STRCMP('abcd','QRSTUV') abcd_QRSTUV,
+STRCMP('qrstuv','QRSTUV') qrstuv_QRSTUV,
+STRCMP('12345','xyz') 12345_xyz,
+STRCMP('xyz','qrstuv') xyz_qrstuv;
+"""
+
+
+# 
+query = """
+UPDATE string_tbl
+SET text_fld = CONCAT('- START -',text_fld, '- EXTRA -');
+"""
+
+query = """
+SELECT *
+FROM string_tbl;
+"""
+
+
+# Exercise 7-1
+# Write a query that returns the 17th through 25th characters of the string 'Please
+# find the substring in this string'.
+query = """
+SELECT SUBSTRING('Please find the substring in this string', 17, 25);"""
+
+# Exercise 7-2
+# Write a query that returns the absolute value and sign (−1, 0, or 1) of the number
+# −25.76823. Also return the number rounded to the nearest hundredth.
+query = """
+SELECT SIGN(-25.76823);
+"""
+
+# Exercise 7-3
+# Write a query to return just the month portion of the current date.
+query = """
+SELECT EXTRACT(YEAR FROM CURRENT_DATE());
+"""
+
+
+# Execute query
+results = executor.execute_query(query)
+
+
+print(results)
+executor.close()
